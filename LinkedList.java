b80baf5f49eb111491fd16c985f7717bed7f2229
BashairAlsadhan <bashair.sadhan@gmail.com>
2023-09-29 19:56:21 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -2,7 +2,6 @@
     //linked list data structure used to store the contacts.
     //methods for adding, searching, and deleting contacts from the list.
 
-    //public boolean add(){}
     // public Contact -or list of contacts?- search(){}
     // public void delete(){ also deletes events }
 public class LinkedList<T>  { 
